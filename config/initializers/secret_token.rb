e151c7eb40eaf952502942bb50d3e9a2085bb39a
Cliff Hales <cliff.junk@gmail.com>
2012-07-23 10:00:57 -0500

    Initial rails scaffold, with Mongoid support added.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EagleTracks::Application.config.secret_token = '551b3f29939e4c065e35c269809506cad843896471b577ffa6daf3629d22a053dd1bebef08227bfaaacc69bfa67d925e2070fb035c728960449802327828cb95'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+EagleTracks::Application.config.session_store :cookie_store, key: '_EagleTracks_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# EagleTracks::Application.config.session_store :active_record_store
